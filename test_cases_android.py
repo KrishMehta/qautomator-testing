ee3555897089d7590be055396bdfc6fc9a00cfa2
Krish Mehta <me@krishvmehta.com>
2024-07-29 15:11:44 +0200

    feat: Update import order in test_cases_android.py

--- test_cases_android.py
+++ test_cases_android.py
@@ -1,6 +1,6 @@
 from appium import webdriver
-from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
